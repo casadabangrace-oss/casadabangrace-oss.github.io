61418308150ed72941ce48967852a31b378ee66a
casadabangrace-oss <casadabangrace@gmail.com>
2026-09-24 21:33:52 +0000

    update

--- enchanted-runner.js
+++ enchanted-runner.js
@@ -1,14 +1,25 @@
-//Find the things we need from the webpage
 const startButton = document.querySelector("#start-button");
 const game = document.querySelector("#game");
+const player = document.querySelector("#player");
 
-//Start the game when button is clicked 
 startButton.addEventListener("click", function() {
 
-  //Change the game's state
-  game.classList.remove("not-started");
-  game.classList.add("playing");
+game.classList.remove("not-started");
+game.classList.add("playing");
 
-  //Hide the start button
-  startButton.remove();
+startButton.remove();
+});
+
+document.addEventListener("keydown", function(event) {
+
+if (event.code === "Space") {
+
+player.style.bottom = "150px";
+
+setTimeout(function() {
+player.style.bottom = "55px";
+}, 500);
+
+}
+
 });
